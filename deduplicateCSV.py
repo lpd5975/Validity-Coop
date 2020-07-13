57ceb2127cd7d3fca83498c65ea1db221165cb6b
lpd5975 <lpd5975@rit.edu>
2020-07-13 10:51:04 -0400

    Reading CSV
    Laid the groundwork/pseudocode for reading the CSV file.
    
    Must work on algorithm to find typos/duplicates.

--- deduplicateCSV.py
+++ deduplicateCSV.py
@@ -1,4 +1,23 @@
 import csv
+import json
+
+def getLastNames(csvReader):
+    lastNames = {}
+    dupNames = {}
+    for row in csvReader:
+        currName = row[2]
+        #If dup found:
+	    #add to dup name
+ 	#else:
+	    #add to lastNames
+
+
+def test():
+    csvFileString = input("Enter name of CSV file")
+    with open(csvFileString as csvFile:
+        csvReader = csv.DictReader(csvFile, delimiter = ',')
+        #answer = getLastNames(csvReader)
+             
 
 def main():
     print("Initial Commit")

aa810701390fffc089504de09dde0246024ea00c
lpd5975 <lpd5975@rit.edu>
2020-07-13 12:31:53 -0400

    Added some details
    Added a few comment and utility functions. getLastNames is trickier than I originally thought.
    
    Problem: Written for exact duplicates, but having difficulty orgnaizing the wrong spellings. Possible solution si to add one mroe dict and utility function.
    
    Later Issue: Dealing with missing last names.

--- deduplicateCSV.py
+++ deduplicateCSV.py
@@ -5,44 +5,56 @@ def leven(str_one, str_two):
     str_one_len = len(str_one)
     str_two_len = len(str_two)
     if str_one_len == 0:
-        return str_two_len
+        return str_two_len        # If either string is empty, no need to create matrix
     elif str_two_len == 0:
-        return str_one_len
+        return str_one_len        # Same as previous note     
     else:
         matrix = [[0 for x in range(str_two_len + 1)] for y in range(str_one_len + 1)]
         for i in range(0, str_one_len + 1):
             for t in range(0, str_two_len + 1):
                 if i == 0:
-                    matrix[i][t] = t
+                    matrix[i][t] = t           # Fills up the first row and first col
                 elif t == 0:
                     matrix[i][t] = i
                 else:
                     cost = 1
                     if str_one[i-1] == str_two[t-1]:
-                        cost = 0
-                    insert = matrix[i][t-1] + 1
-                    delete = matrix[i-1][t] + 1
+                        cost = 0                        # Cost to edit char
+                    insert = matrix[i][t-1] + 1         # If they are the same, no incr
+                    delete = matrix[i-1][t] + 1         #  from previous cost
                     substitue = matrix[i-1][t-1] + cost
                     matrix[i][t] = min(insert, delete, substitue)
     return matrix[str_one_len][str_two_len]
+
+def probablyTypo(str_one, str_two):
+    return leven(str_one.lower(), str_two.lower()) < 3       # A typo/duplicate name will
+                                                             #  most likely be below 3 edits
+
+def findTypos(currName, lastNames):
+    for key in lastNames:
+        if probablyTypo(currName, key):
+            return (True, key)
+    return (False, "False") 
 
 def getLastNames(csvReader):
     lastNames = {}
     dupNames = {}
     for row in csvReader:
         currName = row[2]
-        #If dup found:
-	    #add to dup name
- 	#else:
-	    #add to lastNames
+        If currName in lastNames:
+            dupNames[currName] = row
+            lastNames.pop(currName)
+ 	else:
+	    lastNames[currName] = row
 
 
 def test():
     csvFileString = input("Enter name of CSV file")
     with open(csvFileString) as csvFile:
         csvReader = csv.DictReader(csvFile, delimiter = ',')
-        #answer = getLastNames(csvReader)
-             
+        lastNames = getLastNames(csvReader)
+        
+                     
 
 def main():
     print(leven("sun", "sunny"))
